d153a9e906d76b3db25e7cd43321f4cb20ef1383
Ryan Scott <rcscott@users.noreply.github.com>
2020-09-12 08:53:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mortgage Payoff Length Calculator
+
+Use this tool to help answer the question "how much faster could I be done with my mortgage if I just paid an extra X dollars per month?"
+
+https://rcscott.github.io/mortgage-payoff-length/
---
+++ index.js
@@ -0,0 +1,98 @@
+var app = new Vue({
+  el: '#app',
+
+  data: {
+    mortgageAmount: 290000,
+    interestRate: 3.50,
+    monthlyPaymentMin: 1300,
+    monthlyPaymentMax: 2300,
+    paymentIncrement: 100,
+    paymentsData: [],
+    chart: null,
+  },
+
+  mounted: function () {
+    this.calculatePayments();
+  },
+
+  watch: {
+    paymentsData: function (newPaymentsData) {
+      let chartLabels = _.map(newPaymentsData, function (p) { return `$${p.monthlyPayment}`; });
+      let chartData = _.map(newPaymentsData, 'numberOfPayments');
+
+      if (_.isNull(this.chart)) {
+        this.chart = new Chart($('#chart'), {
+          type: 'bar',
+          data: {
+            labels: chartLabels,
+            datasets: [{
+              label: '# of Months',
+              data: chartData,
+              backgroundColor: '#17a2b8',
+            }],
+          },
+          options: {
+            title: {
+              display: true,
+              text: 'Monthly Payment Amount vs. Number of Months',
+            },
+            legend: { display: false },
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true,
+                },
+              }],
+              xAxes: [{
+                scaleLabel: {
+                  display: true,
+                  labelString: 'Monthly Payment Amount',
+                },
+              }],
+            },
+          },
+        });
+      } else {
+        this.chart.data.labels = chartLabels;
+        this.chart.data.datasets[0].data = chartData;
+        this.chart.update();
+      }
+    },
+  },
+
+  methods: {
+    calculatePayments: function () {
+      let paymentRange = _.range(
+        this.monthlyPaymentMin, this.monthlyPaymentMax + this.paymentIncrement, this.paymentIncrement);
+
+      this.paymentsData = _.map(paymentRange, function (paymentAmount) {
+        let mortgageBalance = this.mortgageAmount;
+        let monthlyInterestRate = this.interestRate / 100 / 12;
+        let paymentCount = 0;
+        let finalPayment;
+
+        while (mortgageBalance > 0) {
+          paymentCount += 1;
+          mortgageBalance = mortgageBalance + (mortgageBalance * monthlyInterestRate);
+
+          if (paymentAmount > mortgageBalance) {
+            finalPayment = mortgageBalance;
+          }
+
+          mortgageBalance -= paymentAmount;
+        }
+
+        const payoffYears = Math.floor(paymentCount / 12);
+        const payoffMonths = paymentCount % 12;
+
+        return {
+          monthlyPayment: paymentAmount,
+          numberOfPayments: paymentCount,
+          payoffYears: payoffYears,
+          payoffMonths: payoffMonths,
+          finalPayment: _.round(finalPayment, 2)
+        };
+      }.bind(this));
+    },
+  },
+});
